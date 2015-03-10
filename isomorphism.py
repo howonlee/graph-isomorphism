e170e260ad8271bc8ae24c032646b14d4dace39d
Howon Lee <howon@howonlee.com>
2015-03-09 22:04:44 -0700

    isomorphism mostly poked at

--- isomorphism.py
+++ isomorphism.py
@@ -38,8 +38,16 @@ def hash_degn(deg_n):
         if val > 1:
             print "what the fuck"
 
+def reverse_node_labels(net1):
+    net2 = nx.Graph()
+    len_nodes = len(net1.nodes())
+    net2.add_nodes_from(range(len_nodes))
+    for node_a, node_b in net1.edges_iter():
+        net2.add_edge(len_nodes - 1 - node_a, len_nodes - 1 - node_b)
+    return net2
+
 if __name__ == "__main__":
-    net = nx.barabasi_albert_graph(6000,15)
-    deg = degrees(net)
-    deg_n = degree_neighborhoods(net, deg)
-    hash_degn(deg_n)
+    net1 = nx.barabasi_albert_graph(6000,15)
+    net2 = reverse_node_labels(net1)
+    deg = degrees(net2)
+    deg_n = degree_neighborhoods(net2, deg)
